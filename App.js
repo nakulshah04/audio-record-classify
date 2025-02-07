1ea24a464b82580d6dbd2d46ee37036009343222
nakulshah04 <shahnakul@gmail.com>
2025-02-07 09:49:36 -0500

    Added playback functionality

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { View, Text, StyleSheet} from 'react-native';
-import useAudioLevels from './sensors/expo_av';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AudioRecorder from './sensors/AudioRecorder'
 
 export default function App() {
-  const decibels = useAudioLevels();
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Audio Level: {decibels ? `${decibels.toFixed(2)} dB` : 'Measuring...'}</Text>
+    <View style={styles.container}>
+      <AudioRecorder />
     </View>
   );
 }
@@ -17,8 +16,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 24,
   },
 });
